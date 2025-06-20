fcebb973fd5d60a334fdcca561cb1269fe0e5f9f
Mariano Fresno <mnofresno+github@gmail.com>
2025-06-20 14:56:11 -0300

    feat: add --load option to docker build command for improved image handling

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build . -f docker/Dockerfile -t ghcr.io/mnofresno/scheduler-mcp:latest --load
+docker build . -f docker/Dockerfile -t ghcr.io/mnofresno/scheduler-mcp:0.0.1 --load
 
-docker push ghcr.io/mnofresno/scheduler-mcp:latest
+docker push ghcr.io/mnofresno/scheduler-mcp:0.0.1
